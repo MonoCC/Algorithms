41b8ce1e76d469c49066702f837a372ef20b4b92
chongdi.yang <chongdi.yang@outlook.com>
2016-10-16 09:39:48 +0800

    添加选择排序方法，优化减少交换次数

--- sorting-algorithm/src/main/java/com/xiaodaima/Selection.java
+++ sorting-algorithm/src/main/java/com/xiaodaima/Selection.java
@@ -2,11 +2,30 @@ package com.xiaodaima;
 
 /**
  * 选择排序
+ * 选择排序的特性：
+ * 1. 选出最小的数与第一位交换，（也就是占用第一位）；选出第二小的数与第二位交换，依次类推
  */
 public class Selection extends AbstractSort implements Sortable {
 
     @Override
     public void sort(int[] s) {
+        for(int i=0; i<s.length - 1; i++) {
+            int minIndex = i;
+            int min = s[i];
+            for(int j=i+1; j<s.length; j++) {
+                if(min > s[j]) {
+                    min = s[j];
+                    minIndex = j;
+                }
+            }
+            if(minIndex != i) {
+                s[minIndex] = s[i];
+                s[i] = min;
+            }
+        }
+    }
 
+    public static void main(String[] args) {
+        new Selection().testSort();
     }
 }
